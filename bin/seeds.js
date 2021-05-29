a32f3848f37b3c1b4a2b583db46d6e5a9379d83c
chloe toussaint <toussaintchloe@hotmail.fr>
2021-05-29 16:05:56 +0200

    routes bikes et detail

--- bin/seeds.js
+++ bin/seeds.js
@@ -46,7 +46,37 @@ const bikes = [{
   description: "Vélo Sunn cintre ville",
   image : "https://saikle-prod.fra1.digitaloceanspaces.com/large_20210527_151637_edfb21cf41.jpg"
 
-}];
+},
+{proprietaire:"_idUser",
+  marque: "Peugeot",
+  type: "Route",
+  location: "75015",
+  etat: "bon etat",
+  taille: "S",
+  description: "Vélo Peugeot Cannes",
+  image : "https://saikle-prod.fra1.digitaloceanspaces.com/large_902_F6658_6716_46_EF_B312_C4_D6_A0_D3_F51_B_ca562b4592.jpeg"
+},
+{proprietaire:"_idUser",
+  marque: "Gitane",
+  type: "Course",
+  location: "75009",
+  etat: "bon etat",
+  taille: "L",
+  description: "Vélo course Gitane « champion »",
+  image : "https://saikle-prod.fra1.digitaloceanspaces.com/large_BE_712913_2860_4137_9347_C2_F6_BFD_6256_C_f94c601888.jpeg"
+
+},
+{proprietaire:"_idUser",
+  marque: "MBK",
+  type: "Course",
+  location: "75007",
+  etat: "bon etat",
+  taille: "M",
+  description: "MBK Trainer - Édition Limitée J.O 1992»",
+  image : "https://saikle-prod.fra1.digitaloceanspaces.com/large_E99_AE_8_AE_6_A3_E_4801_A6_A9_18123_A42_F904_64fbe51de4.jpeg"
+
+}
+];
 
 Bike.insertMany(bikes)
   .then(function (bikesFromDB) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const Bike = require ("../models/Bike.model.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/bikes", (req, res, next) => {
+	Bike.find().then(allBikes => {
+    allBikes.forEach((b)=>console.log(b));
+		res.render("bike", { bikes: allBikes })
+	}).catch(error => console.log(error))
+});
+
+router.get("/bikes/:id", (req, res, next) => {
+  
+  Bike.findOne({ _id: req.params.id })
+    .then(oneBike => {
+      console.log (oneBike)
+     res.render("bike-detail", {
+        unVelo: oneBike
+      })
+  
+    })
+    .catch(error => console.log(error))
+})
+
+
+
 
 module.exports = router;
